e763b7dd5a4ef4efdb4841cf386209895a804fe8
Nikolay Martoplyas <kolyan94martoplyas@gmail.com>
2024-08-03 14:13:43 +0300

    JUnit 4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,37 +1,41 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
-import org.testng.Assert;
+import org.junit.Test;
+import org.junit.Assert;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
+
     @Test
-    public void negativePurchaseAmount(){
+    public void negativePurchaseAmount() {
         int actual = service.remain(-100);
         int expected = 1_100;
 
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
     }
+
     @Test
-    public void positivePurchaseAmount (){
+    public void positivePurchaseAmount() {
         int actual = service.remain(1_100);
         int expected = 900;
 
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
     }
+
     @Test
-    public void zeroPurchaseAmount (){
+    public void zeroPurchaseAmount() {
         int actual = service.remain(0);
         int expected = 1_000;
 
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
     }
+
     @Test
-    public void purchaseWithBonusAmount (){
+    public void purchaseWithBonusAmount() {
         int actual = service.remain(1_000);
         int expected = 0;
 
-        Assert.assertEquals(actual, expected);
+        Assert.assertEquals(expected, actual);
     }
 
 }
